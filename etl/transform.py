58271d3678622de1798dcaa359a19efc4cb37d10
Cleiton Silva <cleitonsilvasilveira67@gmail.com>
2026-04-30 19:22:40 -0300

    Transformação dos dados e acrescentando colunas calculadas

--- etl/transform.py
+++ etl/transform.py
@@ -1,5 +1,6 @@
 import extract as ext
 import pandas as pd
+import numpy as np
 
 def transformar_dados():
     # Extração silenciosa
@@ -13,21 +14,108 @@ def transformar_dados():
     df_baixas['tipo'] = 'Baixa'
     df_b3 = pd.concat([df_altas, df_baixas]).reset_index(drop=True)
 
-    # Imprime apenas o resultado final que você quer
-    print("\n[TABELA B3 TOP 7 MAIORES ALTAS]")
-    print(df_altas)
+    # Renomeando a coluna 'tipo' para algo melhor
+    df_b3.rename(columns={'tipo': 'tendencia_dia'}, inplace=True)
 
-    print("\n[TABELA B3 TOP 7 MAIORES BAIXAS]")
-    print(df_baixas)
+
+    # --- CRIAÇÃO DA COLUNA DE VARIAÇÃO NOMINAL ---
+    # Aplico a fórmula: Preco / (1 + (Variacao/100)) para achar o preço anterior
+    # Depois subtraímos do preço atual para ter a diferença em Reais
+
+    df_b3['variacao_rs'] = df_b3['preco'] - (df_b3['preco'] / (1 + (df_b3['variacao'] / 100)))
     
-    print("\n[TABELA CRIPTOS]")
-    print(df_cripto)
+    # Arredondando para 2 casas decimais
+    df_b3['variacao_rs'] = df_b3['variacao_rs'].round(2)
 
-    print("\n[TABELA MOEDAS]")
-    print(df_moedas)
 
-    print("\n[TABELA MACROS]")
-    print(df_macro)
+    # --- PADRONIZAÇÃO B3 (Ações) ---
+    condicoes_b3 = [
+        (df_b3['variacao'] > 3),            # Subiu muito
+        (df_b3['variacao'] < -3),           # Caiu muito
+        (df_b3['variacao'].between(-1, 1))  # Quase não mudou
+    ]
 
-if __name__ == "__main__":
-    transformar_dados()
+    categorias_b3 = [
+        'Alta Relevante',   # Em vez de Alta Volatilidade
+        'Queda Relevante',  # Em vez de Baixa Volatilidade
+        'Estável / Sem Tendência'
+    ]
+
+    df_b3['status_mercado'] = np.select(condicoes_b3, categorias_b3, default='Oscilação Comum')
+
+    # Criptos:
+    df_cripto['variacao_rs'] = df_cripto['preco_brl'] - (df_cripto['preco_brl'] / (1 + (df_cripto['variacao'] / 100)))
+    df_cripto['variacao_rs'] = df_cripto['variacao_rs'].round(2)
+
+    # --- PADRONIZAÇÃO CRIPTO ---
+    condicoes_cripto = [
+        (df_cripto['variacao'] > 7),            # Subiu muito
+        (df_cripto['variacao'] < -7),           # Caiu muito
+        (df_cripto['variacao'].between(-2, 2))  # Quase não mudou
+    ]
+
+    categorias_cripto = [
+        'Forte Alta',
+        'Forte Queda',
+        'Estável'
+    ]
+    df_cripto['status_mercado'] = np.select(condicoes_cripto, categorias_cripto, default='Oscilação Comum')
+
+    return df_b3, df_macro, df_moedas, df_cripto
+
+b3, macro, moedas, cripto = transformar_dados()
+
+
+# TRATAMENTO B3
+#print(b3.dtypes)
+print(b3)
+
+
+#TRATAMENTO MACROS
+#print(macro.dtypes)
+#print(macro)
+
+# mudando a data de str para datetime64
+macro['data'] = pd.to_datetime(macro['data'], dayfirst=True)
+
+#print(macro.dtypes)
+#print(macro)
+
+# MOEDAS
+#print(moedas.dtypes)
+#print(moedas)
+
+moedas['data'] = pd.to_datetime(moedas['data'], dayfirst=True)
+#print (moedas)
+#print(moedas.dtypes)
+
+# CRIPTOS
+#print(cripto.dtypes)
+print(cripto)
+
+cripto['data'] = pd.to_datetime(cripto['data'], dayfirst=True)
+#print(cripto)
+#print(cripto.dtypes)
+
+
+
+
+
+    # Imprime apenas o resultado final que você quer
+    #print("\n[TABELA B3 TOP 7 MAIORES ALTAS]")
+    #print(df_altas)
+
+    #print("\n[TABELA B3 TOP 7 MAIORES BAIXAS]")
+    #print(df_baixas)
+    
+    #print("\n[TABELA CRIPTOS]")
+    #print(df_cripto)
+
+    #print("\n[TABELA MOEDAS]")
+    #print(df_moedas)
+
+    #print("\n[TABELA MACROS]")
+    #print(df_macro)
+
+#if __name__ == "__main__":
+#    transformar_dados()
